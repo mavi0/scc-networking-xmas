e6a3b9401c6c9defe73fb479102068e318f8e319
Eleanor Davies <ellie@mavieson.co.uk>
2019-11-20 17:25:34 +0000

    tidy

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import os, errno, glob, sys, smtplib, ssl, json
+import os
+import errno
+import glob
+import sys
+import smtplib
+import ssl
+import json
 
 config = {}
+
 
 def load_json(filename):
     with open(filename) as f:
@@ -15,25 +22,34 @@ def load_config():
         config = load_json("config-default.json")
 
 
-def mail(content, receiver):
+def mail(receiver, subject, content):
     global config
+    content = 'Subject: {}\n\n{}'.format(subject, content)
     server = smtplib.SMTP(config["smtp_server"], config["port"])
     server.sendmail(config["email"], receiver, content)
+
 
 def main():
     load_config()
     choices = load_json("xmas.json")
     for choice in choices:
-        content = "Hello " + choice["Full Name"] + ", \n\nThis is an automated email reminding you of the SCC Networking Group Christmas meal, as well as your menu choices.\n\nDate of event: Tuesday 4th of December at 6:30pm at The White Cross Pub\n\nMenu Choices:\nStarter : " + choice["Starter"] + "\nMain: " + choice["Main"] + "\nDessert: " + choice["Dessert"]
+        content = "Hello " + choice["Full Name"] + \
+            ", \n\nThis is an automated email reminding you of the SCC Networking Group Christmas meal, as well as your menu choices.\n" + \
+            "\nDate of event: Tuesday 4th of December at 6:30pm at The White Cross Pub\n" + \
+            "\nMenu Choices:\nStarter : " + \
+            choice["Starter"] + "\nMain: " + choice["Main"] + \
+            "\nDessert: " + choice["Dessert"]
         if not choice["Additional comment to pass to restaurant"] == "":
-            content = content + "\nAdditional comment to pass to restaurant: " + choice["Additional comment to pass to restaurant"]
-        else: 
+            content = content + "\nAdditional comment to pass to restaurant: " + \
+                choice["Additional comment to pass to restaurant"]
+        else:
             content = content + "\nYou left no additional comment to pass to the resturant."
-        
+
         content = content + "\n\nBest wishes,\n\nEleanorbot"
-        
+
         print(content)
-        mail('Subject: {}\n\n{}'.format("SCC Networking Group Christmas Meal Reminder", content), choice["Email Address"])
+        mail(choice["Email Address"],
+             "SCC Networking Group Christmas Meal Reminder", content)
 
 
 main()
